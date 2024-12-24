e73b3c09b2bad308a7b71c28a559416522633c79
Alexey Filichkin <filichkin.alexey@gmail.com>
2024-12-24 10:49:52 +0300

    change pages views to TemplateView and add custom pages for 403, 404 and 500

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 
+handler404 = 'pages.views.page_not_found'
+handler500 = 'pages.views.server_error'
+
+
 urlpatterns = [
     path('pages/', include('pages.urls', namespace='pages')),
     path('', include('blog.urls', namespace='blog')),
