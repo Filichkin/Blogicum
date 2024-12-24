3d5042768707b7808ea178da46517fd32dc5df04
Alexey Filichkin <filichkin.alexey@gmail.com>
2024-12-24 18:14:49 +0300

    change settings.py

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = 'django-insecure-m##x&j)ns=g9)igjeqafd3zw4b$8ew8kjo73ci3(k34m9rmv9x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
