72e93b7715f4f33c4904263ba9c40577207fe0ec
Alexey Filichkin <filichkin.alexey@gmail.com>
2024-12-27 22:56:13 +0300

    add config.py

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from .config import SECRET_KEY
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m##x&j)ns=g9)igjeqafd3zw4b$8ew8kjo73ci3(k34m9rmv9x'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
